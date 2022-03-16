5a70e22098f4b4d3aa18d31daabc984dfcb35487
Abdullah Baig <rebelschaos@gmail.com>
2022-03-16 17:00:13 +0500

    first commit

--- bot.py
+++ bot.py
@@ -69,6 +69,13 @@ async def on_command_error(context, error):
             color=0xFF5733
         )
         await context.reply(embed=embed)
+    else:
+        embed = discord.Embed(
+            title="Error!",
+            description=str(error).capitalize(),
+            color=0xFF5733
+        )
+        await context.reply(embed=embed)
     raise error
 
 
