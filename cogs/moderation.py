f1c783d4e84121ee4eb67a444a3b38d40bf75563
Abdullah Baig <rebelschaos@gmail.com>
2022-03-16 17:10:19 +0500

    latest

--- cogs/moderation.py
+++ cogs/moderation.py
@@ -105,11 +105,11 @@ class ModerationCog(commands.Cog, name="moderation"):
 
     @commands.command(name="announce",
                       description=f"Announces something in a specific channel. Syntax: {config['bot_prefix']}announce <#channel-name> <thing to announce>")
-    #@commands.has_permissions(administrator=True)
-    async def announce(self, context, channel: discord.TextChannel, content):
+    # @commands.has_permissions(administrator=True)
+    async def announce(self, context, channel: discord.TextChannel, *args):
         await channel.send(embed=discord.Embed(timestamp=datetime.datetime.now(), color=0x541760,
-                                               description=content).set_footer(icon_url=self.bot.user.avatar_url,
-                                                                               text="New Announcement "))
+                                               description=args).set_footer(icon_url=self.bot.user.avatar_url,
+                                                                            text="New Announcement "))
 
         await context.send(embed=discord.Embed(timestamp=datetime.datetime.now(), color=0x541760,
                                                description=f"**Operation Successful**\nPosted the new announcement in"
